955a20642036e304d0a308d9c7534d78f3a8962b
Hedi Naili_Automation_Testing <hednailiAutomation@gmail.com>
2024-10-09 15:55:21 +0100

    Update TestBase.java

--- EcommerceProject/src/test/java/tests/TestBase.java
+++ EcommerceProject/src/test/java/tests/TestBase.java
@@ -1,15 +1,11 @@
 package tests;
 
 import java.time.Duration;
-
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 import org.testng.annotations.AfterSuite;
 import org.testng.annotations.BeforeSuite;
-
-import io.github.bonigarcia.wdm.WebDriverManager;
-
 
 public class TestBase {
 
@@ -19,7 +15,6 @@ public class TestBase {
 	public void StartDriver() {
 		ChromeOptions options = new ChromeOptions();
 		options.addArguments("--disable-search-engine-choice-screen");
-		WebDriverManager.chromedriver().setup();
 		driver= new ChromeDriver(options);
 		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(1));
 		driver.get("https://magento.softwaretestingboard.com/checkout/#shipping");
